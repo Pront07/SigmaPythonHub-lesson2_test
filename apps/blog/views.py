2b663aaaf4d2c281b5b3b8d1591c49650f5d2b20
kwzzo <serhii.kozmych@gmail.com>
2024-03-10 13:36:06 +0200

    add new view 'dislike'

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 
+# Create your views here.
 def index(request):
     posts = Post.objects.all()
-    post_id = Post.objects.count() # Кількість постів(виведення їх в хтмл під назвою post_id)
     create_form = PostForm()
 
     context = {
         'posts': posts,
-        'form': create_form,
-        'post_id':  post_id,}
+        'form': create_form
+    }
+
     return render(request, 'blog/index.html', context)
 
 
 def post(request, post_id):
     # post = Post.objects.get(id=post_id)
-
+    form_comment = CommentForm()
+    form_comment = CommentForm()
     post = get_object_or_404(Post, id=post_id)
-    context = {'post': post,
-               }
+    context = {
+        'post': post,
+        'comment_form': form_comment,
+    }
+
     return render(request, 'blog/post.html', context)
 
 
@@ -30,3 +36,29 @@ def create(request):
         if form.is_valid():
             form.save()
     return redirect('blog:index')
+
+
+def comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            print(comment)
+            comment.post = post
+            comment.save()
+    return redirect('blog:post', post_id=post_id)
+
+
+def like(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.likes += 1
+    post.save()
+    return JsonResponse({'likes': post.likes})
+
+
+def dislike(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.dislikes += 1
+    post.save()
+    return JsonResponse({'dislikes': post.dislikes})
