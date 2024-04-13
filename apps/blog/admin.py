7ec63e77bc489af1a74ac0c97102b25141be8301
kwzzo <serhii.kozmych@gmail.com>
2024-04-13 11:19:34 +0300

    fix admin bags

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,9 +1,16 @@
-from django.contrib import admin
-
 # Register your models here.
 from django.contrib import admin
+from .models import Post, Comment
 
-from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)  # Це потрібно для відображення моделі в адмінці
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'content', 'is_published', 'image', 'author']
+    list_display = ['title', 'content', 'is_published', 'image', 'author']
+    list_filter = ['is_published', 'author']
+    search_fields = ['title', 'content', 'author']
+    list_per_page = 10
+
+
+admin.site.register(Comment)  # Це потрібно для відображення моделі в адмінці
